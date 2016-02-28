26125bac993df0b0f36c76f6713b0d8c83cdce4c
sppandya <sahil.pandya@aurigait.com>
2016-02-28 23:15:12 +0530

    first commit

---
+++ src/components/Vote.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import classNames from 'classnames'
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  getPair: function() {
+    return this.props.pair || [];
+  },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
+  },
+  render: function() {
+    return <div className="voting ui inverted segment">
+      {this.getPair().map(entry =>
+        <button key={entry}
+                className={classNames({'ui disabled button': this.isDisabled()},
+                  {voted: this.hasVotedFor(entry)},
+                  {'ui green inverted button': this.hasVotedFor(entry)})}
+                disabled={this.isDisabled()}
+                onClick={() => this.props.vote(entry)}>
+          <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ?
+            <div className="label">Voted</div> :
+            null}
+        </button>
+      )}
+    </div>;
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css!postcss'
+    },
+    {
+    test: /.*\.(gif|png|jpe?g|svg)$/i,
+    loaders: [
+      'file?hash=sha512&digest=hex&name=[hash].[ext]',
+      'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+    ]
+  },
+  { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+  ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  postcss: function () {
+    return [autoprefixer];
+  }
+};
